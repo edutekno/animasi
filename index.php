d1de8da108840970bdb984de08b3a90eca4f201a
edutekno <54890481+edutekno@users.noreply.github.com>
2021-08-10 15:10:27 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?
+$link=$_GET['link'];
+$newURL='http://teknologipembelajaran.com/animasi/'.$link;
+//header('Location: '.$newURL);
+?>
+<script type='text/javascript'>
+window.open("https://www.facebook.com/teknopembelajaran/", "_blank", strWindowFeatures);
+</script>
+<?
+//die();
+?>
